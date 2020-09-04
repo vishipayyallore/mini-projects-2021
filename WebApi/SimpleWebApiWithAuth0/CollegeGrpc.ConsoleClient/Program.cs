4498a24a8bc9a83317880646c5a1d5f1c09cb2c7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-09-04 09:31:05 +0530

    Initial Version

---
+++ WebApi/SimpleWebApiWithAuth0/CollegeGrpc.ConsoleClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServiceWithAuth0.ClientCredentialsDemo;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using static GrpcServiceWithAuth0.ClientCredentialsDemo.Greeter;
+using static System.Console;
+
+namespace CollegeGrpc.ConsoleClient
+{
+    class Program
+    {
+
+        static private GreeterClient _client;
+        
+        private static IConfiguration _config;
+
+        static protected GreeterClient Client
+        {
+            get
+            {
+                if (_client == null)
+                {
+                    var channel = GrpcChannel.ForAddress(_config["RPCService:ServiceUrl"]);
+                    _client = new GreeterClient(channel);
+                }
+                return _client;
+            }
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            _config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json").Build();
+
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            var headers = new Metadata();
+            var token = "YourToken";
+            headers.Add("Authorization", $"Bearer {token}");
+
+            var reply = await Client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" }, headers: headers);
+            Console.WriteLine("Greeting: " + reply.Message);
+
+            Console.WriteLine("\n\nThank You for using the application. \n\nPress any key ...");
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ WebApi/SimpleWebApiWithAuth0/GrpcServiceWithAuth0.ClientCredentialsDemo/Startup.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GrpcServiceWithAuth0.ClientCredentialsDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Added
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = $"https://{Configuration["Auth0Credentials:Domain"]}/";
+                options.Audience = Configuration["Auth0Credentials:Audience"];
+            });
+
+            services.AddAuthorization();
+
+            services.AddGrpc(options =>
+            {
+                options.EnableDetailedErrors = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // Added two lines
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GreeterService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+    }
+}
