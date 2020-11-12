25d20bb8e8680c2172a6abc2c54ba62f3d7b75ab
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-11-12 22:30:49 +0530

    Professors List Page

--- Projects/fullstack-.netcore-angular/Source/4webapp-reactjs/src/components/professors/ListProfessorsPage.js
+++ Projects/fullstack-.netcore-angular/Source/4webapp-reactjs/src/components/professors/ListProfessorsPage.js
@@ -1,11 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import ClockLoader from "react-spinners/ClockLoader";
+
+import { getAllProfessors } from "../../services/professorsService";
 
 function ListProfessorsPage() {
+
+  const [professors, setProfessors] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (professors.length === 0) {
+      getAllProfessors()
+        .then(_professors => {
+          setProfessors(_professors);
+          setLoading(false);
+        });
+    }
+  }, [professors.length]);
+
   return (
-    <>
-      <h2>List Professors</h2>
-      <p>This app uses React.</p>
-    </>
+    <div>
+      <div className="card shadow mt-2 mb-2">
+        <div className="card-header">
+          <h2 className="PageTitle">Professors List</h2>
+        </div>
+        <div className="card-body">
+          <div>
+            <a href="/add-book" className="float">
+              <i className="fa fa-plus float-margintop"></i>
+            </a>
+          </div>
+          <div className="sweet-loading d-flex justify-content-center">
+            <ClockLoader
+              size={150}
+              color={"#123abc"}
+              loading={loading}
+            />
+          </div>
+          <br></br>
+          <table className='table table-striped table-bordered' hidden={loading} aria-labelledby="tableLabel">
+            <thead className="thead-dark">
+              <tr>
+                <th>Professor Id</th>
+                <th>Name</th>
+                <th>DOJ</th>
+                <th>Teaches</th>
+                <th>Salary</th>
+                <th>Is Phd</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                professors.map(professor => {
+                  return (
+                    <tr key={professor.professorId}>
+                      <td>{professor.professorId}</td>
+                      <td>{professor.name}</td>
+                      <td>{new Date(professor.doj).toLocaleDateString()}</td>
+                      <td>{professor.teaches}</td>
+                      <td>{professor.salary}</td>
+                      <td>{professor.isPhd.toString()}</td>
+                      <th scope="col">
+                        <Link to={"/edit-book/" + professor.professorId} className="btn btn-warning btn-sm ml-2 shadow mr-2">
+                          <i className="fa fa-edit fa-fw" aria-hidden="true"></i> Edit</Link>
+                        <Link to={"/delete-book/" + professor.professorId} className="btn btn-danger btn-sm ml-2 shadow">
+                          <i className="fa fa-trash" aria-hidden="true"></i> Delete</Link>
+                      </th>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div >
   );
 }
 
