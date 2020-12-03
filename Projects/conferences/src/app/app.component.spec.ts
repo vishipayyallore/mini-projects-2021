d764818d0f2ec43118e93e0518a9853d53064dbe
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-12-04 00:07:42 +0530

    Few Changes

--- Projects/conferences/src/app/app.component.spec.ts
+++ Projects/conferences/src/app/app.component.spec.ts
@@ -1,8 +1,8 @@
-/* import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('AppComponent Suite', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -32,5 +32,6 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement;
     // expect(compiled.querySelector('.content span').textContent).toContain('conferences app is running!');
   });
+
 });
- */
+
--- Projects/conferences/src/app/services/calculator.service.spec.ts
+++ Projects/conferences/src/app/services/calculator.service.spec.ts
@@ -1,41 +1,116 @@
+import { TestBed } from "@angular/core/testing";
 import { CalculatorService } from "./calculator.service";
-// import { LoggerService } from "./logger.service";
+import { LoggerService } from "./logger.service";
 
 /*
   1. Pending() specs do not run, but their names will show up in the results as pending.
   2. const logger = new LoggerService();
   2. spyOn(logger, 'log');
+  3. xdescribe() to disable the Suite
+  4. xit() to disable the test case.
+  5. fdescribe() foucs describe, will execute only that suite.
+  6. fit() foucs test, will execute only that test case.
 */
-describe('Calculator Service', () => {
 
-  let calculatorService: CalculatorService;
-  const logger = jasmine.createSpyObj('LoggerService', ['log']);
+describe('Calculator Service Suite', () => {
 
-  beforeEach(() => {
-    calculatorService = new CalculatorService(logger);
+  describe('Calculator Service Suite - Version 1', () => {
+
+    let calculatorService: CalculatorService;
+    const logger = new LoggerService();
+
+    beforeEach(() => {
+      spyOn(logger, 'log');
+      calculatorService = new CalculatorService(logger);
+    });
+
+    it('should add two numbers - Spec', () => {
+      const sum = calculatorService.add(2, 2);
+
+      expect(sum).toBe(4);
+      expect(logger.log).toHaveBeenCalledTimes(1);
+    });
+
+    it('should subtract two numbers - Spec', () => {
+      const results = calculatorService.subtract(4, 2);
+
+      expect(results).toBe(2);
+      expect(logger.log).toHaveBeenCalledTimes(1);
+    });
+
   });
 
-  afterEach(() => {
-    // Resetting the Call Count
-    logger.log.calls.reset();
+  describe('Calculator Service Suite - Version 2', () => {
+
+    let calculatorService: CalculatorService,
+      loggerSpy: any;
+
+    beforeEach(() => {
+      loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+      calculatorService = new CalculatorService(loggerSpy);
+    });
+
+    afterEach(() => {
+      loggerSpy.log.calls.reset(); // Resetting the Call Count
+    });
+
+    it('should add two numbers - Spec', () => {
+      const sum = calculatorService.add(2, 2);
+      const toBeValue = 4;
+
+      expect(sum).toBe(toBeValue, `Results ${sum} did not match the expected ${toBeValue}.`);
+      expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
+    it('should subtract two numbers - Spec', () => {
+      const results = calculatorService.subtract(4, 2);
+      const toBeValue = 2;
+
+      expect(results).toBe(toBeValue, `Results ${results} did not match the expected ${toBeValue}.`);
+      expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
   });
 
-  it('should add two numbers', () => {
-    const sum = calculatorService.add(2, 2);
-    const toBeValue = 4;
+  describe('Calculator Service Suite - Version 3', () => {
 
-    expect(sum).toBe(toBeValue, `Results ${sum} did not match the expected ${toBeValue}.`);
+    let calculatorService: CalculatorService,
+      loggerSpy: any;
 
-    expect(logger.log).toHaveBeenCalledTimes(1);
-  });
+    beforeEach(() => {
+      loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
 
-  it('should subtract two numbers', () => {
-    const results = calculatorService.subtract(4, 2);
-    const toBeValue = 2;
+      TestBed.configureTestingModule({
+        providers: [
+          CalculatorService,
+          { provide: LoggerService, useValue: loggerSpy }
+        ]
+      });
 
-    expect(results).toBe(toBeValue, `Results ${results} did not match the expected ${toBeValue}.`);
+      calculatorService = TestBed.inject(CalculatorService);
+    });
 
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    afterEach(() => {
+      loggerSpy.log.calls.reset(); // Resetting the Call Count
+    });
+
+    it('should add two numbers - Spec', () => {
+      const sum = calculatorService.add(2, 2);
+      const toBeValue = 4;
+
+      expect(sum).toBe(toBeValue, `Results ${sum} did not match the expected ${toBeValue}.`);
+      expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
+    it('should subtract two numbers - Spec', () => {
+      const results = calculatorService.subtract(4, 2);
+      const toBeValue = 2;
+
+      expect(results).toBe(toBeValue, `Results ${results} did not match the expected ${toBeValue}.`);
+      expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
   });
 
 });
+
