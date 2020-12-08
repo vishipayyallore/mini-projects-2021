9920033afc140386b36ede07976c558d6279b8db
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-12-09 00:03:41 +0530

    Few Changes

--- Projects/college-.net5-angular-blazorwasm/Source/4webapp-angular11/src/app/app-routing.module.ts
+++ Projects/college-.net5-angular-blazorwasm/Source/4webapp-angular11/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PageNotfoundComponent } from './components/page-notfound/page-notfound.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'pagenotfound', component: PageNotfoundComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pagenotfound', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Projects/college-.net5-angular-blazorwasm/Source/4webapp-angular11/src/app/app.module.ts
+++ Projects/college-.net5-angular-blazorwasm/Source/4webapp-angular11/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
+import { SideNavbarComponent } from './components/side-navbar/side-navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PageNotfoundComponent } from './components/page-notfound/page-notfound.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavbarComponent,
+    SideNavbarComponent,
+    FooterComponent,
+    PageNotfoundComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
