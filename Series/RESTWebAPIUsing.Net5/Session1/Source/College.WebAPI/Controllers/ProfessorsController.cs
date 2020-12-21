181ba0059d728943d795f96c7114b5994a6b6c7f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-12-21 12:48:00 +0530

    Tested the code.

--- Series/RESTWebAPIUsing.Net5/Session1/Source/College.WebAPI/Controllers/ProfessorsController.cs
+++ Series/RESTWebAPIUsing.Net5/Session1/Source/College.WebAPI/Controllers/ProfessorsController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using College.WebAPI.BLL;
+using College.WebAPI.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace College.WebAPI.Controllers
 {
@@ -8,10 +14,93 @@ namespace College.WebAPI.Controllers
     public class ProfessorsController : ControllerBase
     {
 
-        [HttpGet]
-        public string Get()
+        private readonly ILogger<ProfessorsController> _logger;
+        private readonly ProfessorsSqlBll _professorsSqlBll;
+
+        public ProfessorsController(ILogger<ProfessorsController> logger, ProfessorsSqlBll professorsSqlBll)
         {
-            return "Hello World";
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            _professorsSqlBll = professorsSqlBll ?? throw new ArgumentNullException(nameof(professorsSqlBll));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Professor>>> Get()
+        {
+            _logger.Log(LogLevel.Debug, "Request Received for ProfessorsController::Get");
+
+            IEnumerable<Professor> professors = await _professorsSqlBll.GetAllProfessors()
+                                                        .ConfigureAwait(false);
+
+            _logger.Log(LogLevel.Debug, "Returning the results from ProfessorsController::Get");
+
+            return Ok(professors);
+        }
+
+        [HttpGet("{id}", Name = nameof(GetProfessorById))]
+        public async Task<ActionResult<Professor>> GetProfessorById(Guid id)
+        {
+            _logger.Log(LogLevel.Debug, "Request Received for ProfessorsController::GetProfessorById");
+
+            Professor professor = await _professorsSqlBll.GetProfessorById(id)
+                                            .ConfigureAwait(false);
+
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            _logger.Log(LogLevel.Debug, "Returning the results from ProfessorsController::GetProfessorById");
+
+            return Ok(professor);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Professor>> AddProfessor([FromBody] Professor professor)
+        {
+            _logger.Log(LogLevel.Debug, "Request Received for ProfessorsController::AddProfessor");
+
+            var createdProfessor = await _professorsSqlBll.AddProfessor(professor)
+                                            .ConfigureAwait(false);
+
+            _logger.Log(LogLevel.Debug, "Returning the results from ProfessorsController::AddProfessor");
+
+            return CreatedAtRoute(routeName: nameof(GetProfessorById),
+                                  routeValues: new { id = createdProfessor.ProfessorId },
+                                  value: createdProfessor);
+        }
+
+        // PUT: HTTP 200 / HTTP 204 should imply "resource updated successfully". 
+        [HttpPut]
+        public async Task<ActionResult> UpdateProfessor([FromBody] Professor professor)
+        {
+            _logger.Log(LogLevel.Debug, "Request Received for ProfessorsController::UpdateProfessor");
+
+            var _ = await _professorsSqlBll.UpdateProfessor(professor)
+                            .ConfigureAwait(false);
+
+            _logger.Log(LogLevel.Debug, "Returning the results from ProfessorsController::UpdateProfessor");
+
+            return NoContent();
+        }
+
+        // DELETE: HTTP 200 / HTTP 204 should imply "resource deleted successfully".
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProfessor(Guid id)
+        {
+            _logger.Log(LogLevel.Debug, "Request Received for ProfessorsController::DeleteProfessor");
+
+            var professorDeleted = await _professorsSqlBll.DeleteProfessorById(id)
+                                            .ConfigureAwait(false);
+
+            if (!professorDeleted)
+            {
+                return StatusCode(500, $"Unable to delete Professor with id {id}");
+            }
+
+            _logger.Log(LogLevel.Debug, "Returning the results from ProfessorsController::DeleteProfessor");
+
+            return NoContent();
         }
 
     }
--- Series/RESTWebAPIUsing.Net5/Session1/Source/College.WebAPI/Startup.cs
+++ Series/RESTWebAPIUsing.Net5/Session1/Source/College.WebAPI/Startup.cs
@@ -1,5 +1,10 @@
+using College.Core.Constants;
+using College.WebAPI.BLL;
+using College.WebAPI.DAL;
+using College.WebAPI.DAL.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,12 +26,20 @@ namespace College.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = Name, Version = "v1" });
             });
+
+            // Adding EF Core
+            var connectionString = Configuration[Constants.DataStore.SqlConnectionString];
+            services.AddDbContext<CollegeSqlDbContext>(o => o.UseSqlServer(connectionString));
+
+            // Application Services
+            services.AddScoped<ProfessorsSqlBll>();
+            services.AddScoped<ProfessorsSqlDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
