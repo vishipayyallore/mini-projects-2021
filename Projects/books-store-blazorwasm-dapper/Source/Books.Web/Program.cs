d108c94ce6ff9b8c9441864d3eb292dbdeeda3e8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-04-23 20:38:12 +0530

    First Cut Integration

--- Projects/books-store-blazorwasm-dapper/Source/Books.Web/Program.cs
+++ Projects/books-store-blazorwasm-dapper/Source/Books.Web/Program.cs
@@ -1,11 +1,8 @@
+using Books.Web.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Books.Web
@@ -18,6 +15,9 @@ namespace Books.Web
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddHttpClient<IBookDataService, BookDataService>(client =>
+                client.BaseAddress = new Uri(builder.Configuration["WebApis:Books"]));
 
             await builder.Build().RunAsync();
         }
---
+++ Projects/books-store-blazorwasm-dapper/Source/Books.Web/Services/IBookDataService.cs
@@ -0,0 +1,12 @@
+﻿using Books.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Books.Web.Services
+{
+    public interface IBookDataService
+    {
+        Task<IEnumerable<Book>> GetAllBooks();
+    }
+
+}
