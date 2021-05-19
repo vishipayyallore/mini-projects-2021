875a6f71db2bf5ecbcea918f23f3b1630faef864
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-05-19 20:58:52 +0530

    Initial Version

---
+++ Projects/UserLicenses/Source/UserMgmtStroe/dbo/Tables/UserLicenses.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[UserLicenses]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [UserId] VARCHAR(50) NOT NULL, 
+    [License] VARCHAR(50) NOT NULL, 
+    [LicenseUsedFor] VARCHAR(100) NOT NULL, 
+    [CreatedOn] DATETIME2 DEFAULT GETDATE() NOT NULL, 
+    [IsActive] BIT NOT NULL
+)
