e8e662b0d2049d466d9dac893fbc78dbb5415ee4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-07-02 17:58:41 +0530

    Few more methods

--- Projects/booksstore-api/src/controllers/booksController.js
+++ Projects/booksstore-api/src/controllers/booksController.js
@@ -12,40 +12,40 @@ const getAllBooks = asyncHandler(async (request, response) => {
 
 });
 
-// const addBook = asyncHandler(async (request, response) => {
+const addBook = asyncHandler(async (request, response) => {
 
-// 	console.log(`Input Received: ${JSON.stringify(request.body)}`);
+	console.log(`Input Received: ${JSON.stringify(request.body)}`);
 
-// 	// We need to verify both Author Name and Title
-// 	const isBookValid = bookSchemaValidator.validate(request.body);
+	// We need to verify both Author Name and Title
+	const isBookValid = bookSchemaValidator.validate(request.body);
 
-// 	if (isBookValid.error) {
+	if (isBookValid.error) {
 
-// 		console.log("validation result", isBookValid);
-// 		return response.status(400).json(isBookValid.error);
-// 	}
+		console.log("validation result", isBookValid);
+		return response.status(400).json(isBookValid.error);
+	}
 
-// 	// Verify if book's title with same author already exists.
-// 	const similarBookExist = await Book.findOne({ author: request.body.author, title: request.body.title, language: request.body.language });
+	// Verify if book's title with same author already exists.
+	const similarBookExist = await Book.findOne({ author: request.body.author, title: request.body.title, language: request.body.language });
 
-// 	if (similarBookExist) {
+	if (similarBookExist) {
 
-// 		console.log(`Does Similar Book Exists: ${similarBookExist}`);
-// 		return response.status(400).json(`Book with "${request.body.title}" title exists from "${request.body.author}" author.`);
-// 	}
+		console.log(`Does Similar Book Exists: ${similarBookExist}`);
+		return response.status(400).json(`Book with "${request.body.title}" title exists from "${request.body.author}" author.`);
+	}
 
-// 	try {
+	try {
 
-// 		const book = await Book.create(request.body);
+		const book = await Book.create(request.body);
 
-// 		console.log(`Sending Output: ${JSON.stringify(book)}`);
-// 		return response.status(201).json(book);
+		console.log(`Sending Output: ${JSON.stringify(book)}`);
+		return response.status(201).json(book);
 
-// 	} catch (error) {
-// 		return response.status(500).json(error);
-// 	}
+	} catch (error) {
+		return response.status(500).json(error);
+	}
 
-// });
+});
 
 const doesBookExists = asyncHandler(async (request, response, next) => {
 	console.log(`Using Middleware for finding Book. ${request.params.bookId}`);
@@ -73,40 +73,41 @@ const getBookById = asyncHandler(async (request, response) => {
 		.json(request.book);
 });
 
-// const updateBookById = asyncHandler(async (request, response) => {
+const updateBookById = asyncHandler(async (request, response) => {
 
-// 	console.log(`Book Id: ${JSON.parse(JSON.stringify(request.book))._id} | Complete Book: ${JSON.stringify(request.book)}`);
+	console.log(`Book Id: ${JSON.parse(JSON.stringify(request.book))._id} | Complete Book: ${JSON.stringify(request.book)}`);
 
-// 	Book.findByIdAndUpdate(request.params.bookId, request.body, {
-// 		new: true,
-// 		useFindAndModify: false,
-// 		runValidators: true
-// 	},
-// 		function (error, book) {
-// 			if (error) {
-// 				return response.status(500).json(error);
-// 			} else {
-// 				return response.status(200).json({ 'success': true, 'Message': 'Book updated Successfully', data: book });
-// 			}
-// 		});
+	Book.findByIdAndUpdate(request.params.bookId, request.body, {
+		new: true,
+		useFindAndModify: false,
+		runValidators: true
+	},
+		function (error, book) {
+			if (error) {
+				return response.status(500).json(error);
+			} else {
+				return response.status(200).json({ 'success': true, 'Message': 'Book updated Successfully', data: book });
+			}
+		});
 
-// });
+});
 
-// const deleteBookById = asyncHandler(async (request, response) => {
+const deleteBookById = asyncHandler(async (request, response) => {
 
-// 	console.log(`Book Id: ${JSON.parse(JSON.stringify(request.book))._id} | Completed Book: ${JSON.stringify(request.book)}`);
+	console.log(`Book Id: ${JSON.parse(JSON.stringify(request.book))._id} | Completed Book: ${JSON.stringify(request.book)}`);
 
-// 	Book.findByIdAndDelete(request.book._id, function (error, book) {
-// 		if (error) {
-// 			return response.status(500).json(error);
-// 		}
-// 		else {
-// 			return response.status(204).json({ 'success': true, 'Message': 'Book Deleted Successfully' });
-// 		}
-// 	});
+	Book.findByIdAndDelete(request.book._id, function (error, book) {
+		if (error) {
+			return response.status(500).json(error);
+		}
+		else {
+			return response.status(204).json({ 'success': true, 'Message': 'Book Deleted Successfully' });
+		}
+	});
 
-// });
+});
 
 export {
-	getAllBooks, doesBookExists, getBookById
+	getAllBooks, doesBookExists, getBookById, 
+	addBook, updateBookById, deleteBookById
 };
--- Projects/booksstore-api/src/routes/bookRouter.js
+++ Projects/booksstore-api/src/routes/bookRouter.js
@@ -4,18 +4,20 @@ import express from 'express';
 const bookRouter = express.Router();
 
 import {
-    getAllBooks, doesBookExists, getBookById
+    getAllBooks, doesBookExists, getBookById,
+    addBook, updateBookById, deleteBookById
 } from '../controllers/booksController.js';
 
 // Middleware For Retrieving the Book
 bookRouter.use('/:bookId', doesBookExists);
 
 bookRouter.route('/')
-    .get(getAllBooks);
+    .get(getAllBooks)
+    .post(addBook);
 
 bookRouter.route('/:bookId')
-    .get(getBookById);
-//     .put(updateBookById)
-//     .delete(deleteBookById);
+    .get(getBookById)
+    .put(updateBookById)
+    .delete(deleteBookById);
 
 export default bookRouter;
