9134442089e730dd49a1fe0beed1eeaed749423a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-09-07 12:43:17 +0530

    Initial Version

---
+++ Projects/ng12-products/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sv-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng12-products';
+}
